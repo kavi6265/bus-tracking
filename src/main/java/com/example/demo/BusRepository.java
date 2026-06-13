c1177a24374dc62c8967fc50e0bc4fedbc7f9c67
kavi6265 <akavi6265@gmail.com>
2026-06-13 13:05:34 +0530

    Update BusRepository.java

--- src/main/java/com/example/demo/BusRepository.java
+++ src/main/java/com/example/demo/BusRepository.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import java.util.List;
 public interface BusRepository extends MongoRepository<Bus, String> {
     Bus findByBusNumber(String busNumber);
-    List<Bus> findBySourceIgnoreCaseAndDestinationIgnoreCase(
+    Bus findByBusNumberAndBusName(
+            String busNumber,
+            String busName);
+    List<Bus> findBySourceContainingIgnoreCaseAndDestinationContainingIgnoreCase(
             String source,
-            String destination);
+            String destination
+    );
 }
