4a96637aaf8b7b1ba61b4f641e3a8ea38e68de34
kavi6265 <akavi6265@gmail.com>
2026-06-22 19:40:11 +0530

    Update MapPlaceRepository.java

--- src/main/java/com/example/demo/MapPlaceRepository.java
+++ src/main/java/com/example/demo/MapPlaceRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface MapPlaceRepository extends JpaRepository<MapPlace, Long> {
+@Repository // ✅ Change JpaRepository to MongoRepository
+public interface MapPlaceRepository extends MongoRepository<MapPlace, String> {
 }
